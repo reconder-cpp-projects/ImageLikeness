7e45322dc2c55c612ec351a02f2c864e734f97da
Reconder <sinitsynand@ya.ru>
2021-10-17 21:49:08 +0300

    Solution

--- ImageLikeness.cpp
+++ ImageLikeness.cpp
@@ -1,42 +1,131 @@
-//#include <iostream>
+﻿
 #include "opencv2/core.hpp"
-#include <iostream>
 #ifdef HAVE_OPENCV_XFEATURES2D
 #include "opencv2/highgui.hpp"
 #include "opencv2/features2d.hpp"
 #include "opencv2/xfeatures2d.hpp"
-
+#include <opencv2/core/utils/logger.hpp>
 using namespace cv;
 using namespace cv::xfeatures2d;
-using std::cout;
-using std::endl;
+#include <iostream>
+#include <string>
+
+short compare_images(Mat img1, Mat img2)
+{
+    //Инициализируем SURF(Speeded Up Robust Features) алгоритм с Hessian threshold = 400. Согласно документации хорошим значением является от 300 до 500, так что берем то, что по середине.
+    int hessian_threshold = 400;
+    std::shared_ptr<SURF> detector = SURF::create(hessian_threshold);
+    std::vector<KeyPoint> keypoints1, keypoints2;
+    Mat descriptors1, descriptors2;
+    //Вычисляем keypoints и descriptors для каждого изображения при помощи SURF алгоритма
+    detector->detectAndCompute(img1, noArray(), keypoints1, descriptors1);
+    detector->detectAndCompute(img2, noArray(), keypoints2, descriptors2);
+    /*Создаем инстанс класса, который будет сопоставлять дескрипторы по принципу nearest - neighbor, используя библиотеку FLANN(Fast Library for Approximate Nearest Neighbors).
+      Так как используется SURF, используется Евклидова (L2) дистанция между точками.*/
+    std::shared_ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<DMatch> > matches;
+    matcher->knnMatch(descriptors1, descriptors2, matches, 2);
+    /*Фильтруем совпадения при помощи теста Лове. Он сравнивает дистанции совпадений и оставляет только те, у который отношение самой маленькой дистанции ко второй
+      самой маленькой высоко.*/
+    const float ratio_threshold = 0.7f;
+    long good_matches = 0;
+    for (size_t i = 0; i < matches.size(); i++)
+    {
+        if (matches[i][0].distance < ratio_threshold * matches[i][1].distance)
+        {
+            good_matches++;
+        }
+    }
+    //Вычисляем схожесть изображений как отношение хороших совпадений к количеству минимальному количеству углов(features).
+    float likeness = (100 * good_matches / std::min(keypoints1.size(), keypoints2.size()));
+    return (short)likeness;
+}
+struct Image
+{
+    String filename;
+    Mat img;
+    Image(String fn, Mat i) : filename(fn), img(i) {};
+};
+
+
+struct Comparison
+{
+    String filename1;
+    String filename2;
+    short likeness;
+    Comparison(String& fn1, String& fn2, short ln) : filename1(fn1), filename2(fn2), likeness(ln){};
+
+    Comparison(Image& img1, Image& img2) : filename1(img1.filename), filename2(img2.filename)
+    {
+        likeness = compare_images(img1.img, img2.img);
+    }
+
+    void print()
+    {
+        std::cout << filename1 << " " << filename2 << " " << likeness << "%\n";
+    }
+};
+
+
+
+Image get_image(String input)
+{
+    
+    return Image(input, imread(samples::findFile(input), IMREAD_GRAYSCALE));
+}
+
+std::vector<String> parse_input()
+{
+    std::cout << "Input images' filenames:" << std::endl;
+    std::vector<String> inputs;
+    inputs.reserve(10);
+    String line;
+    std::getline(std::cin, line);
+    line = "00";
+    while (std::getline(std::cin, line) && !line.empty())
+    {
+        inputs.push_back(line);
+    }
+    return inputs;
+}
+
+
+
+
+
 int main(int argc, char* argv[])
 {
-    CommandLineParser parser(argc, argv, "{@input | box.png | input image}");
-    Mat src = imread(samples::findFile(parser.get<String>("@input")), IMREAD_GRAYSCALE);
-    if (src.empty())
+    cv::utils::logging::setLogLevel(utils::logging::LogLevel::LOG_LEVEL_SILENT);
+    int threshold;
+    std::cout << "Enter the threshold:" << std::endl;
+    std::cin >> threshold;
+    auto inputs = parse_input();
+
+    std::vector<Image> images;
+    images.reserve(inputs.size());
+    String blank;
+    for (auto& input : inputs)
     {
-        cout << "Could not open or find the image!\n" << endl;
-        cout << "Usage: " << argv[0] << " <Input image>" << endl;
-        return -1;
+        images.push_back(get_image(inputs.back()));
+        inputs.pop_back();
     }
-    //-- Step 1: Detect the keypoints using SURF Detector
-    int minHessian = 400;
-    Ptr<SURF> detector = SURF::create(minHessian);
-    std::vector<KeyPoint> keypoints;
-    detector->detect(src, keypoints);
-    //-- Draw keypoints
-    Mat img_keypoints;
-    drawKeypoints(src, keypoints, img_keypoints);
-    //-- Show detected (drawn) keypoints
-    imshow("SURF Keypoints", img_keypoints);
-    waitKey();
+    
+    std::vector<Comparison> results;
+    results.reserve(images.size() * (images.size() - 1));
+    for (int i = 0; i < images.size(); i++)
+        for (int j = i + 1; j < images.size(); j++)
+            results.push_back(Comparison(images[i], images[j]));
+
+    for (auto& result : results)
+        if (result.likeness >= threshold) result.print();
+
+
     return 0;
 }
 #else
 int main()
 {
-    std::cout << "This tutorial code needs the xfeatures2d contrib module to be run." << std::endl;
+    std::cout << "You will need the xfeatures2d contrib module to run this app." << std::endl;
     return 0;
 }
 #endif
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-ImageLikeness
+Для выполнения задачи использовались features2d.hpp и xfeatures2d.hpp, которые входят в contrib OpenCV.
+Что бы они были необходимо скачать:
+
+1. https://github.com/opencv/opencv/archive/refs/tags/4.5.4.zip
+2. https://github.com/opencv/opencv_contrib
+3. CMake
+
+Далее нужно сделать:
+
+1. Открыть в GUI CMake папку OpenCV
+2. Сделать конфигурацию под х64
+3. Изменить конфиг, что бы поле OPENCV_EXTRA_MODULES_PATH = {папка contrib}/modules
+4. Поставить OPENCV_ENABLE_NONFREE = true, BUILD_opencv_world = true
+5. По желанию отключить BUILD_opencv_python2, BUILD_opencv_python3, BUILD_opencv_java
+6. Добавить в конфиг string OpenCV_RUNTIME = vc16
+7. Нажать "Configure" и "Generate" 6. Собрать релиз или дебаг:
+   cmake.exe --build . --config Debug --target INSTALL
+   cmake.exe --build . --config Release --target INSTALL
+8. Добавить {папка OpenCV}/build/install/x64/vc16/bin в PATH
+
+В проекте Visual Studio установлено:
+Project -> Properties -> Configuration Properties -> VC++ Directories -> Include Directories добавить {папка OpenCV}/build/install/include
+
+Project -> Properties -> Configuration Properties -> VC++ Directories -> Library Directories добавить {папка OpenCV}/build/install/x64/vc16/lib
+
+Project -> Properties -> Configuration Properties -> Linker -> Input -> Additional Dependencies добавить opencv_world454d.dll
